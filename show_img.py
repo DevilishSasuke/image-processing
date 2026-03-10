96018fe6f4cba96aff8fd6a21a97fe96b898fe8c
Oleg <plugin@cs.karelia.ru>
2026-03-10 19:01:28 +0300

    rework show_img with pyplot

--- show_img.py
+++ show_img.py
@@ -1,5 +1,18 @@
 import cv2
+from matplotlib import pyplot as plt
 
-def show_img(img: cv2.typing.MatLike | None, title: str = ""):
-    cv2.imshow(title, img)
-    cv2.waitKey(0)
+def show_img(img: cv2.typing.MatLike | None, title: str = "", 
+             transformation: int = cv2.COLOR_BGR2RGB, size: int = 10):
+    plt.figure(figsize=(size, size))
+
+    if len(img.shape) == 2:
+        plt.imshow(img, cmap="gray", vmin=0, vmax=255)
+    elif len(img.shape) == 3:
+        img_trnsf = cv2.cvtColor(img, transformation)
+        plt.imshow(img_trnsf)
+
+    plt.title(title)
+    plt.axis("off")
+    #plt.tight_layout(pad=0, w_pad=0, h_pad=0)
+    #plt.margins(x=0, y=0)
+    plt.show()
